a33181d63845a4daaff8b6f491010d65e387555e
Alfonso M. García Astorga <me@alfon.io>
2020-05-28 17:33:51 +0200

    update

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,7 +1,51 @@
+const ENGLISH_ALPHABET = "abcdefghijklmnopqrstuvwxyz".split("");
+
+const BOB_RESPONSES = {
+  QUESTION: "Sure.",
+  YELL: "Whoa, chill out!",
+  ADDRESS: "Fine. Be that way!",
+  ELSE: "Whatever.",
+};
+
+type BOB_RESPONSE_TYPES = "QUESTION" | "YELL" | "ADDRESS" | "ELSE";
+
 class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+  private analyzeStr(str: string): BOB_RESPONSE_TYPES {
+    const isEndingInExclamation = str.charAt(str.length - 1) === "!";
+    const isEndingInQuestionMark = str.charAt(str.length - 1) === "?";
+    const hasAllLettersUppercase =
+      str
+        .replace(" ", "")
+        .toUpperCase()
+        .split("")
+        .filter((c) => ENGLISH_ALPHABET.includes(c))
+        .join("") ===
+      str
+        .replace(" ", "")
+        .split("")
+        .filter((c) => ENGLISH_ALPHABET.includes(c))
+        .join("");
+
+    if (
+      (isEndingInExclamation && hasAllLettersUppercase) ||
+      (hasAllLettersUppercase && !isEndingInQuestionMark)
+    ) {
+      return "YELL";
     }
+    if (isEndingInQuestionMark) {
+      return "QUESTION";
+    }
+
+    return "ELSE";
+  }
+
+  private getBobResponseByType(type: BOB_RESPONSE_TYPES): string {
+    return BOB_RESPONSES[type];
+  }
+
+  hey(str: string): string {
+    return this.getBobResponseByType(this.analyzeStr(str));
+  }
 }
 
-export default Bob
+export default Bob;
