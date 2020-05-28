a87699e5a6c2f63cdc2e75f6f7cfbd58707cfbca
Alfonso M. García Astorga <me@alfon.io>
2020-05-28 13:04:22 +0200

    update

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,17 +1,27 @@
-enum COLOR_CODED_BANDS {
-  black = 0,
-  brown = 1,
-  red = 2,
-  orange = 3,
-  yellow = 4,
-  green = 5,
-  blue = 6,
-  violet = 7,
-  grey = 8,
-  white = 9,
-}
+const COLOR_CODED_BANDS = {
+  BLACK: 0,
+  BROWN: 1,
+  RED: 2,
+  ORANGE: 3,
+  YELLOW: 4,
+  GREEN: 5,
+  BLUE: 6,
+  VIOLET: 7,
+  GREY: 8,
+  WHITE: 9,
+};
 
-type Color = keyof typeof COLOR_CODED_BANDS;
+type Color =
+  | "black"
+  | "brown"
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "violet"
+  | "grey"
+  | "white";
 
 export class ResistorColor {
   private colors: Color[];
@@ -31,7 +41,10 @@ export class ResistorColor {
     return Number(
       this.colors
         .slice(0, 2)
-        .map((c) => COLOR_CODED_BANDS[c])
+        .map(
+          (c) =>
+            COLOR_CODED_BANDS[c.toUpperCase() as keyof typeof COLOR_CODED_BANDS]
+        )
         .join("")
     );
   }
---
+++ typescript/space-age/space-age.ts
@@ -0,0 +1,90 @@
+const SECONDS_IN_ONE_DAY = 60 * 60 * 24;
+
+const EATH_ORBITAL_PERIOD_IN_EARTH_DAYS = 365.25;
+const PLANETS_ORBITAL_PERIOD_IN_EARTH_YEARS = {
+  EARTH: 1,
+  MERCURY: 0.2408467,
+  VENUS: 0.61519726,
+  MARS: 1.8808158,
+  JUPITER: 11.862615,
+  SATURN: 29.447498,
+  URANUS: 84.016846,
+  NEPTUNE: 164.79132,
+};
+
+type PLANET =
+  | "EARTH"
+  | "MERCURY"
+  | "VENUS"
+  | "MARS"
+  | "JUPITER"
+  | "SATURN"
+  | "URANUS"
+  | "NEPTUNE";
+
+class SpaceAge {
+  seconds: number;
+
+  constructor(ageInSeconds: number) {
+    this.seconds = ageInSeconds;
+  }
+
+  private outputAge(age: number): number {
+    return +age.toFixed(2);
+  }
+
+  private getPlanetOrbitalPeriodInEarthDays(planet: PLANET): number {
+    return (
+      PLANETS_ORBITAL_PERIOD_IN_EARTH_YEARS[planet] *
+      EATH_ORBITAL_PERIOD_IN_EARTH_DAYS
+    );
+  }
+
+  private calculateAgeInPlanet(planet: PLANET): number {
+    const ageInDays = this.seconds / SECONDS_IN_ONE_DAY;
+
+    switch (planet) {
+      case "EARTH":
+        return this.outputAge(ageInDays / EATH_ORBITAL_PERIOD_IN_EARTH_DAYS);
+
+      default:
+        return this.outputAge(
+          ageInDays / this.getPlanetOrbitalPeriodInEarthDays(planet)
+        );
+    }
+  }
+
+  onEarth(): number {
+    return this.calculateAgeInPlanet("EARTH");
+  }
+
+  onMercury(): number {
+    return this.calculateAgeInPlanet("MERCURY");
+  }
+
+  onVenus(): number {
+    return this.calculateAgeInPlanet("VENUS");
+  }
+
+  onMars(): number {
+    return this.calculateAgeInPlanet("MARS");
+  }
+
+  onJupiter(): number {
+    return this.calculateAgeInPlanet("JUPITER");
+  }
+
+  onSaturn(): number {
+    return this.calculateAgeInPlanet("SATURN");
+  }
+
+  onUranus(): number {
+    return this.calculateAgeInPlanet("URANUS");
+  }
+
+  onNeptune(): number {
+    return this.calculateAgeInPlanet("NEPTUNE");
+  }
+}
+
+export default SpaceAge;
