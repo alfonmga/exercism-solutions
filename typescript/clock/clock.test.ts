508bf2a3283003fb1a3b8b177fa15957cd3c67d0
Alfonso M. García Astorga <me@alfon.io>
2020-05-29 13:59:53 +0200

    update

--- typescript/clock/clock.test.ts
+++ typescript/clock/clock.test.ts
@@ -66,17 +66,17 @@ describe("Clock", () => {
       expect(new Clock(1, -160).toString()).toEqual("22:20");
     });
 
-    // test("negative minutes rolls over continuously", () => {
-    //   expect(new Clock(1, -4820).toString()).toEqual("16:40");
-    // });
+    test("negative minutes rolls over continuously", () => {
+      expect(new Clock(1, -4820).toString()).toEqual("16:40");
+    });
 
-    // test('negative hour and minutes both roll over', () => {
-    //   expect(new Clock(-25, -160).toString()).toEqual('20:20')
-    // })
+    test("negative hour and minutes both roll over", () => {
+      expect(new Clock(-25, -160).toString()).toEqual("20:20");
+    });
 
-    // test('negative hour and minutes both roll over continuously', () => {
-    //   expect(new Clock(-121, -5810).toString()).toEqual('22:10')
-    // })
+    test("negative hour and minutes both roll over continuously", () => {
+      expect(new Clock(-121, -5810).toString()).toEqual("22:10");
+    });
 
     describe("Adding and subtracting minutes", () => {
       test("add minutes", () => {
@@ -103,78 +103,78 @@ describe("Clock", () => {
       test("add more than two days", () => {
         expect(new Clock(1, 1).plus(3500).toString()).toEqual("11:21");
       });
-      // test('subtract minutes', () => {
-      //   expect(new Clock(10, 3).minus(3).toString()).toEqual('10:00')
-      // })
-      // test('subtract to previous hour', () => {
-      //   expect(new Clock(10, 3).minus(30).toString()).toEqual('09:33')
-      // })
-      // test('subtract more than an hour', () => {
-      //   expect(new Clock(10, 3).minus(70).toString()).toEqual('08:53')
-      // })
-      // test('subtract across midnight', () => {
-      //   expect(new Clock(0, 3).minus(4).toString()).toEqual('23:59')
-      // })
-      // test('subtract more than two hours', () => {
-      //   expect(new Clock(0, 0).minus(160).toString()).toEqual('21:20')
-      // })
-      // test('subtract more than two hours with borrow', () => {
-      //   expect(new Clock(6, 15).minus(160).toString()).toEqual('03:35')
-      // })
-      // test('subtract more than one day (1500 min = 25 hrs)', () => {
-      //   expect(new Clock(5, 32).minus(1500).toString()).toEqual('04:32')
-      // })
-      // test('subtract more than two days', () => {
-      //   expect(new Clock(2, 20).minus(3000).toString()).toEqual('00:20')
-      // })
+      test("subtract minutes", () => {
+        expect(new Clock(10, 3).minus(3).toString()).toEqual("10:00");
+      });
+      test("subtract to previous hour", () => {
+        expect(new Clock(10, 3).minus(30).toString()).toEqual("09:33");
+      });
+      test("subtract more than an hour", () => {
+        expect(new Clock(10, 3).minus(70).toString()).toEqual("08:53");
+      });
+      test("subtract across midnight", () => {
+        expect(new Clock(0, 3).minus(4).toString()).toEqual("23:59");
+      });
+      test("subtract more than two hours", () => {
+        expect(new Clock(0, 0).minus(160).toString()).toEqual("21:20");
+      });
+      test("subtract more than two hours with borrow", () => {
+        expect(new Clock(6, 15).minus(160).toString()).toEqual("03:35");
+      });
+      test("subtract more than one day (1500 min = 25 hrs)", () => {
+        expect(new Clock(5, 32).minus(1500).toString()).toEqual("04:32");
+      });
+      test("subtract more than two days", () => {
+        expect(new Clock(2, 20).minus(3000).toString()).toEqual("00:20");
+      });
     });
 
     describe("Construct two separate clocks, set times, test if they are equal", () => {
-      // test('clocks with same time', () => {
-      //   expect(new Clock(15, 37).equals(new Clock(15, 37))).toBeTruthy()
-      // })
-      // test('clocks a minute apart', () => {
-      //   expect(new Clock(15, 36).equals(new Clock(15, 37))).toBeFalsy()
-      // })
-      // test('clocks an hour apart', () => {
-      //   expect(new Clock(14, 37).equals(new Clock(15, 37))).toBeFalsy()
-      // })
-      // test('clocks with hour overflow', () => {
-      //   expect(new Clock(10, 37).equals(new Clock(34, 37))).toBeTruthy()
-      // })
-      // test('clocks with hour overflow by several days', () => {
-      //   expect(new Clock(3, 11).equals(new Clock(99, 11))).toBeTruthy()
-      // })
-      // test('clocks with negative hour', () => {
-      //   expect(new Clock(22, 40).equals(new Clock(-2, 40))).toBeTruthy()
-      // })
-      // test('clocks with negative hour that wraps', () => {
-      //   expect(new Clock(17, 3).equals(new Clock(-31, 3))).toBeTruthy()
-      // })
-      // test('clocks with negative hour that wraps multiple times', () => {
-      //   expect(new Clock(13, 49).equals(new Clock(-83, 49))).toBeTruthy()
-      // })
-      // test('clocks with minute overflow', () => {
-      //   expect(new Clock(0, 1).equals(new Clock(0, 1441))).toBeTruthy()
-      // })
-      // test('clocks with minute overflow by several days', () => {
-      //   expect(new Clock(2, 2).equals(new Clock(2, 4322))).toBeTruthy()
-      // })
-      // test('clocks with negative minute', () => {
-      //   expect(new Clock(2, 40).equals(new Clock(3, -20))).toBeTruthy()
-      // })
-      // test('clocks with negative minute that wraps', () => {
-      //   expect(new Clock(4, 10).equals(new Clock(5, -1490))).toBeTruthy()
-      // })
-      // test('clocks with negative minute that wraps multiple times', () => {
-      //   expect(new Clock(6, 15).equals(new Clock(6, -4305))).toBeTruthy()
-      // })
-      // test('clocks with negative hours and minutes', () => {
-      //   expect(new Clock(7, 32).equals(new Clock(-12, -268))).toBeTruthy()
-      // })
-      // test('clocks with negative hours and minutes that wrap', () => {
-      //   expect(new Clock(18, 7).equals(new Clock(-54, -11513))).toBeTruthy()
-      // })
+      test("clocks with same time", () => {
+        expect(new Clock(15, 37).equals(new Clock(15, 37))).toBeTruthy();
+      });
+      test("clocks a minute apart", () => {
+        expect(new Clock(15, 36).equals(new Clock(15, 37))).toBeFalsy();
+      });
+      test("clocks an hour apart", () => {
+        expect(new Clock(14, 37).equals(new Clock(15, 37))).toBeFalsy();
+      });
+      test("clocks with hour overflow", () => {
+        expect(new Clock(10, 37).equals(new Clock(34, 37))).toBeTruthy();
+      });
+      test("clocks with hour overflow by several days", () => {
+        expect(new Clock(3, 11).equals(new Clock(99, 11))).toBeTruthy();
+      });
+      test("clocks with negative hour", () => {
+        expect(new Clock(22, 40).equals(new Clock(-2, 40))).toBeTruthy();
+      });
+      test("clocks with negative hour that wraps", () => {
+        expect(new Clock(17, 3).equals(new Clock(-31, 3))).toBeTruthy();
+      });
+      test("clocks with negative hour that wraps multiple times", () => {
+        expect(new Clock(13, 49).equals(new Clock(-83, 49))).toBeTruthy();
+      });
+      test("clocks with minute overflow", () => {
+        expect(new Clock(0, 1).equals(new Clock(0, 1441))).toBeTruthy();
+      });
+      test("clocks with minute overflow by several days", () => {
+        expect(new Clock(2, 2).equals(new Clock(2, 4322))).toBeTruthy();
+      });
+      test("clocks with negative minute", () => {
+        expect(new Clock(2, 40).equals(new Clock(3, -20))).toBeTruthy();
+      });
+      test("clocks with negative minute that wraps", () => {
+        expect(new Clock(4, 10).equals(new Clock(5, -1490))).toBeTruthy();
+      });
+      test("clocks with negative minute that wraps multiple times", () => {
+        expect(new Clock(6, 15).equals(new Clock(6, -4305))).toBeTruthy();
+      });
+      test("clocks with negative hours and minutes", () => {
+        expect(new Clock(7, 32).equals(new Clock(-12, -268))).toBeTruthy();
+      });
+      test("clocks with negative hours and minutes that wrap", () => {
+        expect(new Clock(18, 7).equals(new Clock(-54, -11513))).toBeTruthy();
+      });
     });
   });
 });
--- typescript/clock/clock.ts
+++ typescript/clock/clock.ts
@@ -1,74 +1,26 @@
-// @TODO: Refactor to use Date object
 class Clock {
-  hour: number;
-  minutes: number = 0;
+  time: Date;
 
   constructor(initialHour: number, initialMinutes?: number) {
-    this.hour = initialHour;
-
-    if (initialMinutes) {
-      this.minutes = initialMinutes;
-    }
-
-    this.adjustHours();
-    this.adjustMinutes();
-  }
-
-  adjustHours() {
-    if (0 < this.hour && this.hour < 25) {
-      return;
-    }
-
-    const newHour = Math.floor(this.hour % 24);
-    this.hour = newHour < 0 ? Math.abs(24 - Math.abs(newHour)) : newHour;
-  }
-
-  adjustMinutes() {
-    if (0 < this.minutes && this.minutes < 60) {
-      return;
-    }
-
-    const extraHoursInCurrentMinutes = Math.floor(this.minutes / 60);
-    this.hour +=
-      (extraHoursInCurrentMinutes + 24 * (extraHoursInCurrentMinutes % 24)) %
-      24;
-
-    this.minutes = Math.round(
-      (this.minutes / 60 - extraHoursInCurrentMinutes) * 60
-    );
+    this.time = new Date();
+    this.time.setHours(initialHour, initialMinutes || 0);
   }
 
   plus(minutes: number): Clock {
-    this.minutes += minutes;
-    this.adjustHours();
-    this.adjustMinutes();
+    this.time.setTime(this.time.getTime() + minutes * 60 * 1000);
     return this;
   }
   minus(minutes: number): Clock {
-    this.minutes -= minutes;
-    this.adjustHours();
-    this.adjustMinutes();
+    this.time.setTime(this.time.getTime() - minutes * 60 * 1000);
     return this;
   }
   equals(clock: Clock): boolean {
-    return this === clock;
+    return this.toString() === clock.toString();
   }
-
-  private outputHour(): string {
-    let currentHour = this.hour;
-    if (currentHour === 24) {
-      currentHour = 0;
-    }
-
-    return currentHour >= 10 ? currentHour.toString() : `0${currentHour}`;
-  }
-
-  private outputMinutes(): string {
-    return this.minutes >= 10 ? this.minutes.toString() : `0${this.minutes}`;
-  }
-
   toString(): string {
-    return `${this.outputHour()}:${this.outputMinutes()}`;
+    return `${String(this.time.getHours()).padStart(2, "0")}:${String(
+      this.time.getMinutes()
+    ).padStart(2, "0")}`;
   }
 }
 
