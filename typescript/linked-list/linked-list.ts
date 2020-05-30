b8e532bf67a3fad29df7537914d50eac42433ccd
Alfonso M. García Astorga <me@alfon.io>
2020-05-30 22:40:58 +0200

    update

---
+++ typescript/linked-list/linked-list.ts
@@ -0,0 +1,14 @@
+class LinkedList<T> {
+  push(value: T): void {}
+  //   pop(): number {
+  //     return 1;
+  //   }
+  //   shift(): void {}
+  //   unshift(pos: number): void {}
+  //   count(): number {
+  //     return 1;
+  //   }
+  //   delete(pos: number): void {}
+}
+
+export default LinkedList;
